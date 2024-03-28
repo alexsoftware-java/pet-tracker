ba8536b4bf3e895dfba515aed27fd0798cfdcea3
Alexey Khlyzov <alex.khlizov@gmail.com>
2024-03-28 15:45:22 +0300

    small fix of test

--- src/test/java/com/some/pettracker/service/PetServiceTest.java
+++ src/test/java/com/some/pettracker/service/PetServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,23 +21,16 @@ class PetServiceTest {
     @MockBean
     private PetRepository petRepository;
 
-    private PetService petService;
-
     @Test
     void testGetPetsOutsideZone() {
         // given
-        petService = new PetService(petRepository, new PetConverter());
+        PetService petService = new PetService(petRepository, new PetConverter());
         PetEntity petEntity1 = new PetEntity();
         petEntity1.setPetType(PetType.CAT);
         petEntity1.setTrackerType(TrackerType.SMALL_CAT);
         petEntity1.setOwnerId(1);
         petEntity1.setInZone(false); // Outside the zone
-        PetEntity petEntity2 = new PetEntity();
-        petEntity2.setPetType(PetType.CAT);
-        petEntity2.setTrackerType(TrackerType.BIG_CAT);
-        petEntity2.setOwnerId(2);
-        petEntity2.setInZone(true); // Inside the zone
-        when(petRepository.findAllByInZoneFalse()).thenReturn(Arrays.asList(petEntity1, petEntity2));
+        when(petRepository.findAllByInZoneFalse()).thenReturn(List.of(petEntity1));
 
         // when
         List<PetTypeDto> result = petService.getPetsOutsideZone();
@@ -46,7 +38,7 @@ class PetServiceTest {
         // then
         assertEquals(1, result.size());
         assertEquals(PetType.CAT, result.get(0).getPetType());
-        assertEquals(2, result.get(0).getTrackerTypes().size());
+        assertEquals(1, result.get(0).getTrackerTypes().size());
         assertEquals(TrackerType.SMALL_CAT, result.get(0).getTrackerTypes().get(0).getTrackerType());
 
         verify(petRepository, times(1)).findAllByInZoneFalse();
